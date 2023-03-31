d2d2785fa117fd2cfc190870981b97c5ae11eed9
Alexandre <unconfigured@null.spigotmc.org>
2023-03-31 13:51:07 +0200

    feat: create and update user in surrealDB

--- services/users.tsx
+++ services/users.tsx
@@ -1,40 +1,88 @@
-export async function getUser(uid: string): Promise<User | any> {
+import { connectSurreal, db } from "../surrealdb";
+
+export async function getUser(uid: string): Promise<User> {
+  console.log("Getting user from SurrealDB");
+  console.log("Input uid : " + uid);
   let user: User = {
-    uid: "",
+    uid: uid,
+    id: "",
     name: "",
-    address: "",
   };
 
-  // const res = await fireDB.collection("user").where("uid", "==", uid).get();
+  const accountDetails: AccountDetails = await connectSurreal().then(
+    async () => {
+      console.log("Quering SurrealDB for account:" + uid);
+      const res = await db.select("account:" + uid);
+      console.log("accountDetails res : ");
+      console.log(res);
+      console.log(typeof res);
 
-  // res.forEach((elem) => {
-  //   // console.log(elem.data());
-  //   const stringValue = JSON.stringify(elem.data());
-  //   user = {
-  //     ...JSON.parse(stringValue),
-  //     id: elem.id,
-  //   };
-  // });
-  // return user;
-  return {};
+      if (
+        (res[0] as AccountDetails).created_at &&
+        (res[0] as AccountDetails).id &&
+        (res[0] as AccountDetails).name
+      ) {
+        console.log("Type checked");
+        console.log(typeof res[0]);
+        console.log(res[0]);
+        const accountDetails: AccountDetails = JSON.parse(
+          JSON.stringify(res[0])
+        );
+        console.log("Account details : ");
+        console.log(accountDetails);
+        return accountDetails;
+      } else {
+        throw new Error("Invalid response type");
+      }
+    }
+  );
+
+  user.id = accountDetails.id;
+  user.name = accountDetails.name;
+  user.profilePicture = accountDetails.profilePicture;
+
+  console.log("Got user from SurrealDB");
+  return user;
 }
 
-export function createUser(user: User): void {
-  // fireDB
-  //   .collection("user")
-  //   .add(user)
-  //   .then(() => {
-  //     console.log("User created");
-  //   })
-  //   .catch(() => {
-  //     console.log("Error user creation");
-  //   });
+export async function createUser(user: User): Promise<void> {
+  console.log("Creating user in SurrealDB");
+  console.log("Input user data : " + JSON.stringify(user));
+  await connectSurreal().then(async () => {
+    console.log("Quering SurrealDB for account:" + user.uid);
+    const res = await db.create("account:" + user.uid, {
+      name: user.name,
+      created_at: new Date().toISOString(),
+    });
+    console.log("Create user response : " + JSON.stringify(res));
+  });
+}
+
+export async function updateUser(user: User): Promise<void> {
+  console.log("Updating user in SurrealDB");
+  console.log("Input user data : " + JSON.stringify(user));
+  await connectSurreal().then(async () => {
+    console.log("Quering SurrealDB for " + user.id);
+    const res = await db.change(user.id, {
+      name: user.name,
+      profilePicture: user.profilePicture,
+      updated_at: new Date().toISOString(),
+    });
+    console.log("Update user response : " + JSON.stringify(res));
+  });
 }
 
 export type User = {
-  id?: string;
+  id: string;
   uid: string;
   name: string;
-  address: string;
-  profilePic?: string;
+  profilePicture?: string;
+};
+
+export type AccountDetails = {
+  created_at: string;
+  updated_at?: string;
+  id: string;
+  name: string;
+  profilePicture?: string;
 };
