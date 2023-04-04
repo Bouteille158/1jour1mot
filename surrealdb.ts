7587816b48c197a7ff868293187afd1326a1a592
Alexandre <unconfigured@null.spigotmc.org>
2023-04-04 16:17:23 +0200

    feat: get today date fore word id in SurrealDB

--- surrealdb.ts
+++ surrealdb.ts
@@ -14,4 +14,15 @@ async function connectSurreal() {
   console.log("Connection to Surreal established");
 }
 
+export function getSurrealCompatibleDate(date: Date): string {
+  const year = date.getFullYear().toString().slice(-2);
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+
+  const formattedDate = year + month + day;
+
+  console.log(formattedDate); // affiche une date sous le format YYMMDD (par exemple, 230327 pour le 27 mars 2023)
+  return formattedDate;
+}
+
 export { connectSurreal, db };
