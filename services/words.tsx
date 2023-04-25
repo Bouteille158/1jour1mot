2d8d9ff881dc75f407cf57b8637e7d2a0ea9facb
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2023-04-25 14:31:12 +0200

    feat: create isWordCurrentlyLearned function

--- services/words.tsx
+++ services/words.tsx
@@ -33,7 +33,7 @@ export async function getTodayWordForEndUser(
     throw new Error("User account id is not defined");
   }
 
-  const word = await getCurrentDayWordFromSurreal();
+  let word = await getCurrentDayWordFromSurreal();
 
   console.log("Word first phase : ");
   console.log(word?.word);
@@ -41,10 +41,13 @@ export async function getTodayWordForEndUser(
   if (!word) {
     const newWord = await getNewWordFromDicolink();
     await importNewWordFromDicolink(newWord, userAccountID);
-    const word = await getCurrentDayWordFromSurreal();
+    word = await getCurrentDayWordFromSurreal();
     console.log("Word second phase : ");
     console.log(word?.word);
-    return word as Word;
+  }
+
+  if (!word) {
+    throw new Error("Word is not defined after second phase");
   }
 
   console.log(word.id);
@@ -52,23 +55,17 @@ export async function getTodayWordForEndUser(
     throw new Error("Word id is not defined");
   }
 
-  const learnedWordsList = await getLearningWordListForAccount(userAccountID);
-  console.log("Learned words list : ");
-  console.log(learnedWordsList);
-
-  if (!learnedWordsList) {
-    console.log("Learned words list is null");
-  } else if (learnedWordsList instanceof Error) {
-    console.error("Error while getting learned words list");
-    alert("Error while getting learned words list");
-    throw new Error("Error while getting learned words list");
-  }
-
-  if (!learnedWordsList.includes(word.id)) {
-    console.log("Word is not in learned list");
-    addWordToLearnedWordsList(word.id, userAccountID);
-  } else {
-    console.log("Word is in learned list");
+  try {
+    const isLearningWord = await isWordCurrentlyLearned(userAccountID, word.id);
+    if (!isLearningWord) {
+      console.log("Word is not learned yet =============================");
+      addWordToLearnedWordsList(word.id, userAccountID);
+    } else {
+      console.log("Word is already learned =============================");
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 
   return word as Word;
@@ -138,6 +135,43 @@ export async function addWordToLearnedWordsList(
     console.log("Add word to learned words list : ");
     console.log(res);
     return;
+  });
+}
+
+export async function isWordCurrentlyLearned(
+  accountID: string,
+  wordID: string
+) {
+  console.log("Account ID : " + accountID);
+  console.log("Word ID : " + wordID);
+  return await connectSurreal().then(async () => {
+    const res: RelateResponse<LearnsObject> | Error = await db
+      .query(
+        "SELECT * FROM learns WHERE out == " +
+          wordID +
+          " AND in == " +
+          accountID
+      )
+      .then((res: any) => {
+        console.log(JSON.stringify(res));
+        if (res[0].status === "ERR") {
+          console.error("Error while getting word relation : " + res[0]);
+          throw new Error("Error while getting word relation : " + res[0]);
+        }
+        return res[0] as RelateResponse<LearnsObject>;
+      })
+      .catch((err) => {
+        console.error(err);
+        return new Error(err);
+      });
+
+    if (!res) {
+      return new Error("Error while getting word relation : res is empty");
+    } else if (res instanceof Error) {
+      return new Error("Error while getting word relation : " + res);
+    }
+
+    return res.result.length ? true : false;
   });
 }
 
@@ -220,6 +254,14 @@ export interface RelateResponse<T> {
   time: string;
   status: string;
   result: [T];
+}
+
+export interface LearnsObject {
+  created_at: string;
+  guessedRight: boolean;
+  id: string;
+  in: string;
+  out: string;
 }
 
 export interface LearnsRelation {
