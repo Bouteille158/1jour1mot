6591a932f4d2bde56672edd3055f2c6d95db1817
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2023-04-28 12:05:14 +0200

    feat: create word guess screen

--- screens/WordGuessScreen.tsx
+++ screens/WordGuessScreen.tsx
@@ -1,64 +1,80 @@
 import { useEffect, useState } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import Spacer from "../components/Spacer";
-import ShopItem from "../components/ShopItem";
 import { Text, View } from "../components/Themed";
-import { getShoes, Shoe } from "../services/shoes";
+import InputCustom from "../components/InputCustom";
+import ButtonCustom from "../components/ButtonCustom";
+
+import { useHeaderHeight } from "@react-navigation/elements";
 
 export default function WordGuessScreen() {
-  const [shoes, setShoes] = useState<Shoe[]>([]);
-  useEffect(() => {
-    getShoes().then((res) => {
-      console.log("res: ", res);
-      setShoes(res);
-      console.log("Shoes : ", shoes);
-    });
-  }, []);
+  const [guess, setGuess] = useState<string>("");
+  useEffect(() => {}, []);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      // borderWidth: 2,
+      // borderColor: "#00ccff",
+    },
+    title: {
+      // borderWidth: 2,
+      // borderColor: "#275745",
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+  });
+  const headerHeight = useHeaderHeight();
+
   return (
-    <View style={styles.container}>
-      <Spacer height={20} />
-      <Text style={styles.title}>WIP</Text>
-      <Spacer height={5} />
-      <FlatList
-        data={shoes}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.shoeList}
-        keyExtractor={(_) => _.id}
-        renderItem={({ item, index }) => {
-          return <ShopItem key={index} shoe={item}></ShopItem>;
-        }}
-        ItemSeparatorComponent={() => {
-          return <Spacer height={20}></Spacer>;
-        }}
-      />
-    </View>
+    <KeyboardAvoidingView
+      style={{
+        flex: 1,
+        // borderWidth: 2,
+        // borderColor: "#ff0000ff"
+      }}
+      behavior={Platform.select({ android: undefined, ios: "padding" })}
+      keyboardVerticalOffset={Platform.select({
+        android: headerHeight + 2000,
+        ios: headerHeight - 200,
+      })}
+    >
+      {/* <ScrollView bounces={false}> */}
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Devinez le dernier mot que vous avez appris
+        </Text>
+        <Spacer height={30} />
+        <InputCustom
+          value={guess}
+          onChange={function (value: string): void {
+            setGuess(value);
+          }}
+          placeholder="Entrez un mot"
+          width={"80%"}
+        ></InputCustom>
+        <Text>
+          {guess.length > 0
+            ? "Vous avez entré : " + guess
+            : "Vous n'avez pas encore entré de mot"}
+        </Text>
+        <Spacer height={10}></Spacer>
+        <ButtonCustom
+          onPress={() => {
+            console.log("Button pressed");
+          }}
+        >
+          Valider
+        </ButtonCustom>
+      </View>
+      {/* </ScrollView> */}
+    </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    // borderWidth: 2,
-    // borderColor: "#00ccff",
-  },
-  scroll: {
-    flex: 1,
-    // borderWidth: 2,
-    // borderColor: "#000",
-  },
-  title: {
-    // borderWidth: 2,
-    // borderColor: "#275745",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  shoeList: {
-    // width: "100%",
-    // height: "100%",
-    // borderWidth: 2,
-    // borderColor: "#03f137",
-    paddingBottom: 20,
-  },
-});
